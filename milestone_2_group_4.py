a068de2484f1946a3cbe8ca3e08ca3700777b9a9
bossmanTM <chase2jackman@gmail.com>
2025-11-03 10:27:53 -0700

    init repo

---
+++ milestone_2_group_4.py
@@ -0,0 +1,38 @@
+def main():
+    new_input = input("do you want to start a new game or load an existing one")
+
+
+def new_game():
+    pass
+
+
+def existing_game():
+    pass
+
+
+def game_loop():
+    pass
+
+
+class Stack:
+    pass
+
+
+class Tower(Stack):
+    pass
+
+
+class Hanoi(Tower):
+    def transfer(self, from_: int, to: int) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return f""
+
+    def is_complete(self) -> bool:
+        return False
+
+    def towers(self) -> None:
+        """prints the towers in the game window"""
+
+        pass
