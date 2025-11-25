e591282874b3b670815a5551e6401fb5f154b32f
samsone4 <samsone4@mymacewan.ca>
2025-11-25 06:24:22 -0700

    Update milestone_2_group_4.py

--- milestone_2_group_4.py
+++ milestone_2_group_4.py
@@ -1,14 +1,14 @@
 from milestone_1_group_4 import Hanoi 
 from graphics import GraphWin
 
-def game_loop(game:Hanoi):
+def game_loop(game: Hanoi):
     """
     purpose: handle each iteration of the game
     parameters: game: The Hanoi board to be played
     return: None
     """
     window = GraphWin("Towers of Hanoi")
-    draw_Game(window, game)
+    draw_game(window, game)
     draw_background(window)
     pass
 
@@ -22,7 +22,7 @@ def save_game(filename: str, game: Hanoi):
     """
     pass
 
-def load_game(filename:str):
+def load_game(filename: str):
     """
     purpose: load the game using a given filename
     parameters: filename: the name of the savefile to load
@@ -30,7 +30,7 @@ def load_game(filename:str):
     """
     pass 
 
-def draw_Game(window:GraphWin, game: Hanoi):
+def draw_game(window: GraphWin, game: Hanoi):
     """
     purpose: draw the game onto screen
     parameters: 
@@ -43,12 +43,44 @@ def draw_Game(window:GraphWin, game: Hanoi):
 
 def draw_background(window: GraphWin):
     """
-    purpose: draw the background of the game
-    parameters: window: the window to be edited
-    return: None
+    Purpose: To draw some of the graphics objects that do not change on the window
+    Parameters: The GraphWin object called "window"
+    Return: None
     """
-    
-    return 
+    # Initialize and draw the static graphics objects
+    disk = Text(Point(230, 40), "Number of Disks? (Enter a positive int: 3 by default)")
+    target = Text(Point(220, 75), "Target Tower? (Enter a positive int: 3 by default)") 
+    line = Line(Point(50, 450), Point(850, 450))
+    source = Text(Point(100, 540), "From tower?")
+    destination = Text(Point(210, 540), "to tower?")   
+    static = (disk, destination, line, source, target) 
+    for item in static:
+        item.draw(window)
 
-def main():
-    pass
+def draw_towers(window: GraphWin):
+    """
+    Purpose: To draw the towers on the window
+    Parameters: The GraphWin object called "window"
+    Return: The tuple of the list called "towers"
+    """    
+    # Initialize and draw the towers with spacing in-between
+    towers = []
+    for tower_num in range(1, 4):
+        tower_x = tower_num * 220
+        tower = Rectangle(Point(tower_x, 250), Point((tower_x + 5), 450))
+        tower.setFill("red")
+        tower.setOutline("black")
+        towers.append(tower)
+        tower.draw(window)    
+    return tuple(towers)    
+
+def main(): # Testing window output
+    window = GraphWin("Hanoi Towers Game", 900, 600)
+    draw_static(window)
+    towers = draw_towers(window)
+    try:
+        cursor = window.getMouse()
+    except:
+        window.close()
+
+main()
