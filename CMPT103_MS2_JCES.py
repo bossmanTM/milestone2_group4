bd728d51c6f1a932d5c2e57fe59e0e2b431812c3
samsone4 <samsone4@mymacewan.ca>
2025-12-09 19:04:06 -0700

    Added note

--- CMPT103_MS2_JCES.py
+++ CMPT103_MS2_JCES.py
@@ -6,7 +6,13 @@
 from graphics import *
 from CMPT103_MS2_Classes_JCES import *
 import pickle
- 
+
+"""
+Note: If you change the number of disks and the target tower in the entries, 
+press the "Reset" button to apply those changes. This is to prevent changing the
+number of disks and the target tower during the game.
+"""
+
 def main():
     """
     Purpose: To start the game by initializing and drawing the necessary elements
@@ -19,4 +25,5 @@ def main():
         running = game.frame_update()
 
 main()
+
 
